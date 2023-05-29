52db017c07ab36faf733ecd515409b40152c23c2
Ali <alisham93@bk.ru>
2023-05-30 01:35:55 +0200

    Initial Commit

---
+++ LeclonecoinApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeclonecoinApp
+//
+//  Created by Эл on 30.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
